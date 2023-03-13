774055cb44f424ae8350634e04e4411321611f04
Marin <koenigsegg.matin@gmail.com>
2023-03-13 17:17:46 +0330

    features section and sticky navbar

--- js/main.js
+++ js/main.js
@@ -1,7 +1,17 @@
-const $ = document
+const $ = document;
 
-const navMenuBtn = $.querySelector("#nav-menu-btn")
+const header = $.querySelector("#header");
+const navMenuBtn = $.querySelector("#nav-menu-btn");
 
-navMenuBtn.addEventListener("click", ()=> {
-    
-})
+navMenuBtn.addEventListener("click", () => {});
+
+function stickyNav() {
+  if (document.documentElement.scrollTop > 0) {
+    header.classList.add("sticky-nav");
+  } else {
+    header.classList.remove("sticky-nav");
+  }
+}
+
+document.addEventListener("scroll", stickyNav);
+window.addEventListener("load", stickyNav);
